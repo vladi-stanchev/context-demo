b4a9969a75886beaac06f1c92fd09b730062b025
Vladi <vsstanchev@gmail.com>
2023-08-18 09:49:27 +0100

    Add UserContext

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import Secret from "./Secret";
-import Nav from "./Nav";
-import Footer from "./Footer";
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Secret from './Secret';
+import Nav from './Nav';
+import Footer from './Footer';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-          <Nav />
+    const [loggedIn, setLoggedIn] = useState(false);
+    return (
+        <div className='App'>
+            <BrowserRouter>
+                <UserContext.Provider>
+                    <Nav />
 
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/secret" element={<Secret />} />
-          </Routes>
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/secret' element={<Secret />} />
+                    </Routes>
 
-          <Footer />
-      </BrowserRouter>
-    </div>
-  );
+                    <Footer />
+                </UserContext.Provider>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
